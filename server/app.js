00e23e3c23603a34cc3c36e4787c0c8f5a1168cb
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-14 15:41:12 -0600

    baseline functionality

--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
 // initial jokes provided by the client
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 var jokes = [
   {
     whoseJoke: "Huck",
@@ -16,3 +20,30 @@ var jokes = [
     punchLine: "If you pee on them they disappear."
   }
 ];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/jokes', function(req, res) {
+  console.log("handling jokes request");
+  res.send(jokes);
+});
+
+app.post('/jokes', function (req, res) {
+  console.log("REQ body: ", req.body);
+  var newJoke = req.body;
+  jokes.push(newJoke);
+  res.sendStatus(201);
+});
+
+app.set('port', process.env.PORT || 5000);
+
+app.get("/*", function(req, res) {
+  var file = req.params[0] || "/views/index.html";
+  res.sendFile(path.join(__dirname, "/public/", file))
+});
+app.use(express.static('/public'));
+
+app.listen(app.get('port'), function () {
+  console.log("listening on port " + app.get('port'));
+});
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,53 @@
-console.log('js loaded');
+$(document).ready(function() {
+  console.log('js loaded');
+
+  $('#postJoke').on('submit', function(event) {
+    event.preventDefault();
+    var newJoke = {};
+      $.each($('#postJoke').serializeArray(), function(i, field) {
+        newJoke[field.name] = field.value;
+        $('input[type="text"]').val('');
+      });
+      console.log(newJoke);
+      $.ajax({
+        type: 'POST',
+        url: '/jokes',
+        data: newJoke,
+        success: function(response) {
+          if(response == "Created") {
+            getJokes();
+          } else {
+            alert("no jokes");
+          }
+        }
+      })
+
+  });
+
+});
+
+getJokes();
+
+//functions
+
+  function appendJokes(jokes) {
+    $("#jokeContainer").empty();
+    for (var i = 0; i < jokes.length; i++) {
+      $("#jokeContainer").append("<div class='joke'></div>");
+      var $el = $("#jokeContainer").children().last();
+      $el.append("<h2>" + jokes[i].whoseJoke + "</h2>");
+      $el.append("<p>" + jokes[i].jokeQuestion + "</p>");
+      $el.append("<p>" + jokes[i].punchLine + "</p>");
+    }
+  }
+
+  function getJokes() {
+    $.ajax({
+      type: 'GET',
+      url: '/jokes',
+      success: function(data) {
+        console.log(data);
+        appendJokes(data);
+      }
+    });
+  }
